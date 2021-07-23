0ad696b11437bc5ebb3f51379ebbe616e24a4cdc
tjysdsg <tjy1018543509@163.com>
2021-07-23 14:41:17 +0800

    Add inline docs to openmp_parallel_for.cpp

---
+++ openmp_parallel_for.cpp
@@ -0,0 +1,20 @@
+#include <atomic>
+#include <cstdio>
+
+int main() {
+  int index; /// index in the loop
+  std::atomic<int> team = 0;
+#pragma omp parallel num_threads(3) shared(team) /// team is shared across 3 threads
+  {
+    /// increment-and-get, this is atomic https://en.cppreference.com/w/cpp/atomic/atomic/operator_arith
+    int t = ++team;
+
+#pragma omp for lastprivate(index) /// the latest value of index is kept as the value of index in the outer scope
+    for (int i = 0; i < 18; ++i) {
+      index = i;
+      printf("team: %d; loop index: %d\n", t, i);
+    }
+  }
+  printf("last finished team: %d, index: %d\n", team.load(), index);
+  return 0;
+}
